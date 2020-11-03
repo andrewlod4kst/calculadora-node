54a821b35425578c66a579167a683e5317ee6b0e
andrewlod4kst <andre@4kst.com>
2020-11-03 17:01:46 -0300

    Small fixes

--- src/routes/RouteCalculadora.js
+++ src/routes/RouteCalculadora.js
@@ -1,13 +1,13 @@
 const {Router} = require("express")
 const {ControllerCalculadora} = require("./../controllers")
 const {validate} = require('express-validation')
-const {ValidationNumbers} = require('./../validation')
+const {ValidationNumbers, ValidationDivision} = require('./../validation')
 
 const router = Router()
 
 router.post("/soma", validate(ValidationNumbers), ControllerCalculadora.soma())
 router.post("/sub", validate(ValidationNumbers), ControllerCalculadora.sub())
 router.post("/multi", validate(ValidationNumbers), ControllerCalculadora.multi())
-router.post("/div", validate(ValidationNumbers), ControllerCalculadora.div())
+router.post("/div", validate(ValidationDivision), ControllerCalculadora.div())
 
 module.exports = router
--- src/validation/index.js
+++ src/validation/index.js
@@ -1,3 +1,4 @@
 module.exports = {
-	ValidationNumbers: require("./ValidationNumbers")
+	ValidationNumbers: require("./ValidationNumbers"),
+	ValidationDivision: require("./ValidationDivision")
 }
